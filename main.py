390cafd0a66ad23d585e8e7825a7b4a9855e0e85
fqx <qfuxiang@gmail.com>
2022-01-12 15:09:50 +0800

    change sorting by names

--- main.py
+++ main.py
@@ -22,51 +22,49 @@ if __name__ == '__main__':
     try:
         filename = sys.argv[1]
     except IndexError:
-        print('需要将考勤记录文件拖到程序上！')
+        print('需要将考勤记录文件转成csv拖到程序上！')
         input('按任意键退出。')
 
-    df = pd.read_excel(filename)
+    df = pd.read_csv(filename)
     df['日期时间'] = pd.to_datetime(df['日期时间'], infer_datetime_format=True)
     year = df.loc[0,'日期时间'].year
     month = df.loc[0,'日期时间'].month
     names = set(df['姓名'].unique())
 
-    days = calendar.monthrange(year, month)[1]
-    for i in range(days):
-        day = i+1
+    for name in names:
 
-        # check if is workday
-        if is_workday(datetime(year, month, day)):
-            # 上班
-            attendance = get_attendance(df, beginning=datetime(year, month, day, 0, 0, 0),
-                                        ending=datetime(year, month, day, 9, 10, 0))
-            noshow = names - attendance
-            if noshow:
-                for name in noshow:
+        days = calendar.monthrange(year, month)[1]
+        for i in range(days):
+            day = i+1
+
+            # check if is workday
+            if is_workday(datetime(year, month, day)):
+                # 上班
+                attendance = get_attendance(df, beginning=datetime(year, month, day, 0, 0, 0),
+                                            ending=datetime(year, month, day, 9, 10, 0))
+                # noshow = name - attendance
+                if name not in attendance:
                     print_noshow(name, day, '上午签到')
 
-            # 中午
-            attendance = get_attendance(df, beginning=datetime(year, month, day, 11, 50, 0),
-                                        ending=datetime(year, month, day, 14, 10, 0), times=2)
-            attendance1 = get_attendance(df, beginning=datetime(year, month, day, 11, 50, 0),
-                                        ending=datetime(year, month, day, 14, 10, 0))
-            noshow = names - attendance1  # 一次打卡都没有
-            noshow1 = names - attendance - noshow  # 有一次打卡
-            if noshow:
-                for name in noshow:
+                # 中午
+                attendance = get_attendance(df, beginning=datetime(year, month, day, 11, 50, 0),
+                                            ending=datetime(year, month, day, 14, 10, 0), times=2)
+                attendance1 = get_attendance(df, beginning=datetime(year, month, day, 11, 50, 0),
+                                            ending=datetime(year, month, day, 14, 10, 0))
+                # noshow = name - attendance1  # 一次打卡都没有
+                # noshow1 = name - attendance - noshow  # 有一次打卡
+                if name not in attendance1:
                     print_noshow(name, day, '上午签退')
                     print_noshow(name, day, '下午签到')
-            if noshow1:  # 有一次打卡的记为缺下午签到
-                for name in noshow1:
+                if name not in attendance:  # 有一次打卡的记为缺下午签到
                     print_noshow(name, day, '下午签到')
 
-            # 下班
-            attendance = get_attendance(df, beginning=datetime(year, month, day, 17, 50, 0),
-                                        ending=datetime(year, month, day, 23, 59, 59))
-            noshow = names - attendance
-            if noshow:
-                for name in noshow:
+                # 下班
+                attendance = get_attendance(df, beginning=datetime(year, month, day, 17, 50, 0),
+                                            ending=datetime(year, month, day, 23, 59, 59))
+                # noshow = name - attendance
+                if name not in attendance:
                     print_noshow(name, day, '下午签退')
-        else:
-            pass
+            else:
+                pass
     input('统计结束，按任意键退出。')
